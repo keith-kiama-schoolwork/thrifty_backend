f2bff9834d09b9c5fa5ccece5b188ce1bab4581d
keithkiama <keithkiama2003@gmail.com>
2023-03-28 14:20:12 +0300

    Created resources and established relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-    has_secure_password
     has_one :account
+    has_many :product_reviews
+    has_one :user_review
+    has_many :products, through: :product_reviews
 end
