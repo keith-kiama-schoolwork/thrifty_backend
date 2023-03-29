170fa6b41cf23df0f38fce151e7f6e45e6c858a6
keithkiama <keithkiama2003@gmail.com>
2023-03-29 21:47:26 +0300

    Allowing cross origin requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+ # Allow cross-origin requests
+  after_action :allow_cross_origin_requests
+
+  private
+
+  # Allow cross-origin requests
+  def allow_cross_origin_requests
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+  end
 end
